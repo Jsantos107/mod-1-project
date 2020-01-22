9ea8cc38316928a98d222fc734d5e3a919ac73bc
Jorge <Neneluis55@gmail.com>
2020-01-22 16:00:33 -0700

    cleaning and add to files.

--- lib/models/player.rb
+++ lib/models/player.rb
@@ -2,14 +2,5 @@ class Player < ActiveRecord::Base
     has_many :games
 
 
-    def self.new_player
-        Player.create(name: gets.chomp,points: 0)
-    end
-
-
-
-    def add_points
-
-    end
 
 end

465871b900c75e140abb287920414ececf116880
ZacharyMayle <zam1998@gmail.com>
2020-01-23 11:37:44 -0700

    got the mvp working, still ugly

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ quest1 = Question.create(:question "What color is the sky?", :points 10, :true_a
 quest2 = Question.create(:question "How long is a footlong?", :points 10, :true_a "1 foot", :false1 "2 feet", :false2 "3 feet", :false3 "4 feet")
 
 
-player1 = Player.create(:name Gabriel, :points 0)
+player1 = Player.create(:name Gabriel)
 
 
 game2 = Game.create(:player player1, :question quest1)
--- lib/models/cli.rb
+++ lib/models/cli.rb
@@ -1,56 +1,31 @@
 class Cli
-    attr_reader :player, :question 
+    attr_reader :player, :question
 
-    def initialize(player, question)
-        @player = player
-        @question = question
-    end
     
-    def self.find_player 
-        new_player = gets.chomp
-        
-        player = Player.create(name:new_player)
-        
-        player
-    end
-    def self.start
+
+    
+    def start
         puts "Welcome to MyFeud!"
         puts " "
         puts "please enter your name"
-
-        player = find_player
-        # new_player = gets.chomp
-        
-        # player = Player.create(name:new_player)
-        
+        @player = Player.create_player
         puts "Welcome #{player.name} lets play!!!" + "\n"
-        
         puts " "
         question
     end
     
-    def self.question
+    def question
         quest1 = Question.all.sample
         puts "#{quest1.question} For #{quest1.points} points."
         quest1.question_layout 
-        quest1.user_answer
+        score = quest1.user_answer
+        add_points(score)
         binding.pry
-        # point = 0 
-
-        # if quest1 > 0
-        #     player
-        # else 
-        #     puts "fuck"
-        # end
     end
 
-    # def quest
-    #     add_points
-    # end
-
-    # def add_points
-
-    #     end
+    def add_points(score)
+        player.score += score
+    end
     
     
 end
